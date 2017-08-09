2334a83e504f8cd27df32a09a324a74930be1560
KeyboardFire <andy@keyboardfire.com>
2017-08-08 22:22:46 -0500

    Make it usable
    A fairly functional IPA keyboard at this point.

--- src/main/java/com.keyboardfire.phontwo/Phontwo.java
+++ src/main/java/com.keyboardfire.phontwo/Phontwo.java
@@ -27,6 +27,15 @@ import android.util.Log;
 public class Phontwo extends InputMethodService
         implements KeyboardView.OnKeyboardActionListener {
 
+    private final static String[][] keyData = {
+        {"i", "y"}, {"ɨ", "ʉ"}, {"ɯ", "u"}, {"b", "p"}, {".", "."}, {"d", "t"}, {"ɖ", "ʈ"}, {"ɟ", "c"}, {"ɡ", "k"}, {"ɢ", "q"}, {"ð", "θ"}, {".", "."},
+        {"ɪ", "ʏ"}, {".", "."}, {".", "ʊ"}, {"m", "."}, {"ɱ", "."}, {"n", "."}, {"ɳ", "."}, {"ɲ", "."}, {"ŋ", "."}, {"ɴ", "."}, {"ɮ", "ɬ"}, {".", "."},
+        {"e", "ø"}, {"ɘ", "ɵ"}, {"ɤ", "o"}, {"ʙ", "."}, {"ⱱ", "."}, {"r", "."}, {"ɽ", "."}, {"ɾ", "."}, {".", "."}, {"ʀ", "."}, {"ʒ", "ʃ"}, {".", "."},
+        {"ɛ", "œ"}, {"ɜ", "ɞ"}, {"ʌ", "ɔ"}, {"β", "ɸ"}, {"v", "f"}, {"z", "s"}, {"ʐ", "ʂ"}, {"ʝ", "ç"}, {"ɣ", "x"}, {"ʁ", "χ"}, {"ʑ", "ɕ"}, {".", "."},
+        {"æ", "."}, {"ɐ", "."}, {".", "."}, {"ə", "."}, {"ʋ", "."}, {"ɹ", "."}, {"ɻ", "."}, {"j", "."}, {"j", "."}, {"ʕ", "ħ"}, {"w", "ʍ"}, {"⌫", "."},
+        {"a", "ɶ"}, {".", "."}, {"ɑ", "ɒ"}, {" ", " "}, {" ", " "}, {"l", "."}, {"ɭ", "."}, {"ʎ", "."}, {"ʟ", "."}, {"ɦ", "h"}, {"ʔ", "ʡ"}, {"⏎", "."}
+    };
+
     private StringBuilder composing = new StringBuilder();
 
     private InputMethodManager inputMethodManager;
@@ -35,6 +44,8 @@ public class Phontwo extends InputMethodService
 
     private KeyboardLayer mainKeyboard;
     private KeyboardLayer currentKeyboard;
+
+    private long pressTime;
 
     @Override public void onCreate() {
         super.onCreate();
@@ -64,34 +75,63 @@ public class Phontwo extends InputMethodService
         // currentKeyboard.setImeOptions(getResources(), attr.imeOptions);
     }
 
-    public void onKey(int kc, int[] kcs) {
-        composing.append(String.valueOf((char)kc));
-        commit(getCurrentInputConnection());
+    @Override public void onPress(int kc) {
+        pressTime = System.currentTimeMillis();
+    }
+    @Override public void onRelease(int kc) {
+        switch (kc) {
+            case 59:
+                if (composing.length() > 0) {
+                    composing.setLength(0);
+                    getCurrentInputConnection().commitText("", 0);
+                } else {
+                    sendRawKey(KeyEvent.KEYCODE_DEL);
+                }
+                break;
+            case 63:
+                commit();
+                sendRawKey(KeyEvent.KEYCODE_SPACE);
+                break;
+            case 71:
+                commit();
+                sendRawKey(KeyEvent.KEYCODE_ENTER);
+                break;
+            default:
+                final long deltaTime = System.currentTimeMillis() - pressTime;
+                commit();
+                composing.append(keyData[kc][deltaTime > 200 ? 1 : 0]);
+                getCurrentInputConnection().setComposingText(composing, 1);
+        }
     }
 
-    public void onText(CharSequence text) {
+    @Override public void onText(CharSequence text) {
         InputConnection ic = getCurrentInputConnection();
         if (ic == null) return;
 
         ic.beginBatchEdit();
-        commit(ic);
+        commit();
         ic.commitText(text, 0);
         ic.endBatchEdit();
     }
 
-    private void commit(InputConnection ic) {
+    private void sendRawKey(int kc) {
+        InputConnection ic = getCurrentInputConnection();
+        ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, kc));
+        ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, kc));
+    }
+
+    private void commit() {
         if (composing.length() > 0) {
-            ic.commitText(composing, composing.length());
+            getCurrentInputConnection().commitText(composing, composing.length());
             composing.setLength(0);
         }
     }
 
-    public void swipeUp() {}
-    public void swipeDown() {}
-    public void swipeLeft() {}
-    public void swipeRight() {}
+    @Override public void swipeUp() {}
+    @Override public void swipeDown() {}
+    @Override public void swipeLeft() {}
+    @Override public void swipeRight() {}
 
-    public void onPress(int kc) {}
-    public void onRelease(int kc) {}
+    @Override public void onKey(int kc, int[] kcs) {}
 
 }
